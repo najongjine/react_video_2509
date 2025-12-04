58d020b8ea0eeee7abeb316ab9e7324190b4a098
najongjine <najongjin3@hotmail.com>
2025-12-04 15:09:21 +0900

    .

--- src/page/VideoUpload.tsx
+++ src/page/VideoUpload.tsx
@@ -3,6 +3,7 @@ import VideoUploaderCompo from "../component/VideoUploaderCompo";
 import { useAuthStore } from "../store/authStore";
 import { useShallow } from "zustand/shallow";
 import { useNavigate } from "react-router-dom";
+import * as utils from "../utils/utils";
 
 export default function VideoUpload() {
   // 1. 상태 읽기 (READ)
@@ -21,10 +22,20 @@ export default function VideoUpload() {
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
+  async function validation() {
+    let result = await utils.verify_token(userInfo?.token ?? "");
+    if (result.includes("인증실패")) {
+      alert(`${result}. 로그인을 다시 해주세요.`);
+      logout();
+      navigate("/login");
+    }
+  }
+
   useEffect(() => {
     /* 로그인 되어있나 체크하기
     로그인 안되있으면, alert 띄우고 로그인 페이지로 보내기
      */
+    validation();
   }, []);
 
   return (
