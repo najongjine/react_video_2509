89e338ed7ef3f5dcd902c8c6db7d6087731e118f
najongjine <najongjin3@hotmail.com>
2025-12-05 13:19:53 +0900

    .

--- src/component/VideoUploaderCompo.tsx
+++ src/component/VideoUploaderCompo.tsx
@@ -1,7 +1,23 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+import { useShallow } from "zustand/shallow";
 
 const VideoUploaderCompo: React.FC = () => {
+  /** authStore 에서 유저 정보 가져오는 코드 붙여넣기 */
+  // 1. 상태 읽기 (READ)
+  // useAuthStore 훅을 통해 현재 상태에서 userInfo를 가져옵니다.
+  // 이 방법은 상태가 바뀔 때만 리렌더링됩니다.
+  const userInfo = useAuthStore((state: any) => state.userInfo);
+
+  // 2. 액션 함수 가져오기 (SET을 위한 함수)
+  const { login, logout } = useAuthStore(
+    useShallow((state: any) => ({
+      login: state?.login,
+      logout: state?.logout,
+    }))
+  );
+
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   const [file, setFile] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
