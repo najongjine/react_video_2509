569349f694ae81e87a7413f4a3904d3d058525da
najongjine <najongjin3@hotmail.com>
2025-12-05 15:05:21 +0900

    .

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,45 +1,17 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  /* inputVal 이라는 바인딩 변수랑 set 함수 만들어 주세요.
-  타입은 string */
-  const [inputVal, setinputVal] = useState<string>("");
-  const [evalResult, setevalResult] = useState<string>("");
-  let dummy = "hello";
-  useEffect(() => {
-    dummy = "bye";
-  }, []);
+  /* 우리가 만든 api 에서 비디오 리스트 가져와서 화면에 뿌리기 */
 
-  function onInputEnter(event: React.KeyboardEvent) {
-    let key = event?.key;
-    if (key == "Enter") {
-      const hasOtherCharacters = /[^0-9+\-*/%]/.test(inputVal);
-
-      if (hasOtherCharacters) {
-        alert("수학 수식만 입력해 주세요");
-        return;
-      }
-      let data = eval(inputVal);
-      setevalResult(data);
-    }
+  async function getVideoList() {
+    //...
   }
+
+  useEffect(() => {}, []);
 
   return (
     <div className="content-margin-padding">
-      <div>
-        <input
-          value={inputVal}
-          onChange={(event) => {
-            let input_value = event?.target?.value ?? "";
-            // 숫자와 +, -, *, /, % 만 허용하는 정규식
-
-            setinputVal(input_value);
-          }}
-          onKeyDown={onInputEnter}
-        />
-        <div> = {evalResult}</div>
-        <div>내가 타이핑 한거: {inputVal}</div>
-      </div>
+      <h1>비디오 리스트</h1>
     </div>
   );
 }
